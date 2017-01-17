f3a51d020c58e543316047263fa52aa3be5618bc
AnTi3z <anti3z@anti3z.ru>
2017-01-18 00:26:15 +0300

    Написан основной парсер

--- BScalc.py
+++ BScalc.py
@@ -1,5 +1,7 @@
 import math
 import queue
+import time
+import re
 
 AllBldngsNames = ('hall', 'storage', 'houses', 'sawmill', 'mine',
 'farm', 'barracks', 'wall', 'trebuchet')
@@ -285,5 +287,39 @@ def doBuyReses(building=getNextUpgrBld(),doUpgr=False):
 def msgParser(text):
     global resources
     global buildings
-    pass
+    global war_bldngs
 
+    res = re.search(r"""(?:Жители\s+(\d+).\n)?
+                        (?:Армия\s+(\d+).\n)?
+                           Золото\s+(\d+).\n
+                           Дерево\s+(\d+).\n
+                           Камень\s+(\d+).\n
+                           Еда\s+(\d+).""", text)
+    if res > 0:
+        resources['gold'] = res.group(2)
+        resources['wood'] = res.group(3)
+        resources['stone'] = res.group(4)
+        resources['food'] = res.group(5)
+        resources['time'] = int(time.time()/60)
+
+    bld = (r"""^Постройки\n
+                   \n
+                  (?:\U0001f3e4\s+(\d+).+\n)?
+                  (?:\U0001f3da\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\U0001f3d8\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\U0001f33b\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\U0001f332\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\u26cf\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\U0001f6e1\s+(\d+).+\s+(\d+)/.+\n)?
+                  (?:\U0001f3f0\s+(\d+).+\s+(\d+)/.+\n)?
+                  \n
+                  Что будем строить\?$""", text)
+    if bld.group(0): buildings['hall']['lvl'] = bld.group(0)
+    if bld.group(1): buildings['storage']['lvl'] = bld.group(1)
+    if bld.group(2): buildings['houses']['lvl'] = bld.group(2)
+    if bld.group(3): buildings['farm']['lvl'] = bld.group(3)
+    if bld.group(4): buildings['sawmill']['lvl'] = bld.group(4)
+    if bld.group(5): buildings['mine']['lvl'] = bld.group(5)
+
+    if bld.group(6): war_bldngs['barracks']['lvl'] = bld.group(6)
+    if bld.group(7): war_bldngs['wall']['lvl'] = bld.group(7)
