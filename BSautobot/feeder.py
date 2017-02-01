b2b3e0fc991a448c867b2a63524403afb2029d98
AnTi3z <anti3z@anti3z.ru>
2017-02-01 13:28:55 +0300

    Оптимизация автоматической закупки еды

--- BSautobot/feeder.py
+++ BSautobot/feeder.py
@@ -10,10 +10,18 @@ def doBuyFood():
     storCapacity = (buildings['Склад']['lvl']*50 + 1000) * buildings['Склад']['lvl']
     foodReserv = min(storCapacity, foodReserv)
     foodNeed = foodReserv - resources['food']
+    #Необходимое количество еды
     if foodNeed < 0: foodNeed = 0
+    #Если золота не хватает - покупаем еду на все деньги
+    elif foodNeed > resources['gold'] / 2:
+        foodNeed = resources['gold'] / 2
+        foodReserv = foodNeed + resources['food']
 
+    #Покупка
     doBuyReses(food=foodNeed)
 
-    if foodConsum * hrsReserv * 60 < foodReserv/2: hrsReserv = foodReserv / (foodConsum * 60)
+    #Корректировка времени на которое расчитан запас еды
+    if foodConsum * hrsReserv * 60 > foodReserv: hrsReserv = foodReserv / (foodConsum * 60)
 
+    #Следующая покупка через половину времени на которое у нас запас
     if AUTOFEED: timer.setFeedTimer(int(hrsReserv*60/2))
