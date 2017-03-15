defe035c6be23de9fcd7085b3ab363e8d387390e
AnTi3z <anti3z@anti3z.ru>
2017-03-15 17:24:14 +0300

    Окончательно избавляемся от cmdQueue

--- BSautobot/queues.py
+++ BSautobot/queues.py
@@ -10,18 +10,9 @@ from . import tools
 logger = logging.getLogger(__name__)
 
 queThrdsLock = threading.Lock()
-cmdQueue = queue.Queue()
 msgQueue = queue.Queue()
 que_stoped = True
 
-
-def cmdQueAdd(cmd):
-    global que_stoped
-
-    cmdQueue.put(cmd)
-    if que_stoped:
-        que_stoped = False
-        queGetNext()
 
 def msgQueAdd(msg):
     global que_stoped
@@ -40,26 +31,8 @@ def queGetNext():
         msg = msgQueue.get()
         globalobjs.SendMsg_cb(msg)
         logger.debug('Send msg: %s', msg)
-    elif not cmdQueue.empty():
-        cmdQueParse()
-        logger.debug('cmdQueParsed')
-        queGetNext()
     else:
         que_stoped = True
-
-def cmdQueParse():
-    cmd, *params = cmdQueue.get()
-
-    logger.debug('cmdQueue: cmd:%s   params:%s',cmd,str(params))
-
-    if cmd == 'build':
-        threading.Thread(target=builder.doUpgrade,args=params).start()
-    elif cmd == 'feed':
-        threading.Thread(target=tools.doBuyFood).start()
-    elif cmd == 'reses':
-        threading.Thread(target=tools.doTargetReses,args=params).start()
-    elif cmd == 'ppl':
-        threading.Thread(target=tools.doAutoPpl,args=params).start()
 
 def wait():
     while not que_stoped:
